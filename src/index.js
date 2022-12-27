d538dd2b53a6b3e3efa8dd813839442c8971d91e
AiofmesApp <info@mail.aiofmes.com>
2022-12-27 14:25:01 +0530

    UPDATE ENV

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-window.widget1Url = "http://localhost:3050";
-window.widget2Url = "http://localhost:3051";
+// window.widget1Url = process?.env?.NEXT_PUBLIC_WIDGET_1_URL || "http://localhost:3050";
+// window.widget2Url = process?.env?.NEXT_PUBLIC_WIDGET_2_URL || "http://localhost:3051";
 
 import("./App");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const Dotenv = require("dotenv-webpack");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
     publicPath: "http://localhost:3045/",
   },
@@ -41,12 +42,19 @@ module.exports = {
   },
 
   plugins: [
+    new Dotenv(),
     new ModuleFederationPlugin({
       name: "shell",
       filename: "remoteEntry.js",
       remotes: {
-        widget1: "widget_1@[window.widget1Url]/remoteEntry.js",
-        widget2: "widget_2@[window.widget2Url]/remoteEntry.js",
+        widget1: `widget_1@${
+          (process && process.env.NEXT_PUBLIC_WIDGET_1_URL) ||
+          "http://localhost:3050"
+        }/remoteEntry.js`,
+        widget2: `widget_2@${
+          (process && process.env.NEXT_PUBLIC_WIDGET_2_URL) ||
+          "http://localhost:3051"
+        }/remoteEntry.js`,
       },
       exposes: {},
       shared: {
@@ -66,4 +74,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
